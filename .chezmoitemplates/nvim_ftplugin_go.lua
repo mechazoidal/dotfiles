524ea16887db5de70e58a48930846d11163e9db3
Scott Francis <scott@kurokoproject.com>
2023-12-29 12:26:13 -0800

    Incorporate changes from dev laptop

--- .chezmoitemplates/nvim_ftplugin_go.lua
+++ .chezmoitemplates/nvim_ftplugin_go.lua
@@ -1,6 +1,17 @@
 vim.lsp.start({
   name = 'gopls',
   cmd = {'gopls'},
-  root_dir = vim.fs.dirname(vim.fs.find({'go.mod', '.git'}, { upward = true })[1]),
   -- lsp-config supports 'single_file_support' for these, but base LSP client does not.
+  root_dir = vim.fs.dirname(vim.fs.find({'go.mod', '.git'}, { upward = true })[1]),
+  settings = {
+    gopls = {
+      -- analyses = {
+      --   unusedparams = true,
+      -- },
+      staticcheck = true,
+    },
+  },
 })
+-- hack for now
+-- ideally we could use setlocal since this is basically a wrapper(:h :CompilerSet)
+vim.cmd('compiler go')
--- dot_config/nvim/lua/user/treesitter.lua
+++ dot_config/nvim/lua/user/treesitter.lua
@@ -6,6 +6,7 @@ require'nvim-treesitter.configs'.setup {
     "dot",
     "go",
     "gomod",
+    "gosum",
     "hcl",
     "javascript",
     "json",
